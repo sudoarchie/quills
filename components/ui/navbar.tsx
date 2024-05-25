482b719384009f34ee04f754d5a67b8a429ca601
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-05-25 22:54:59 +0530

    fixing navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -9,11 +9,15 @@ const menuItems = [
         href: '#',
     },
     {
-        name: 'About',
+        name: 'Products',
         href: '#',
     },
     {
-        name: 'Contact',
+        name: 'Cart',
+        href: '#',
+    },
+    {
+        name: 'About',
         href: '#',
     },
 ]
@@ -69,7 +73,7 @@ export function Navbar() {
                         type="button"
                         className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     >
-                        Button text
+                        Buy Now
                     </button>
                 </div>
                 <div className="lg:hidden">
@@ -127,7 +131,7 @@ export function Navbar() {
                                     type="button"
                                     className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                                 >
-                                    Button text
+                                    Buy Now
                                 </button>
                             </div>
                         </div>
