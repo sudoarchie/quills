7440a464b9d3c94bea094fcb72a8fd77d43c8dcc
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-05-29 15:39:06 +0530

    fixed build issue

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,7 @@ import asthatic3 from "@/public/Designer (21).jpeg"
 import asthatic4 from "@/public/4fb64872-2711-4321-b3ae-feb466a4920f.jpeg"
 import asthatic5 from "@/public/Designer (11).jpeg"
 import { Navbar } from "@/components/ui/navbar";
-import { Globe } from "@/components/ui/globe";
-import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+// import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 export default function Home() {
 return (
   <>
@@ -107,7 +106,7 @@ return (
   ]}></HeroParallax>
   <div>
     <h2 className="text-5xl text-center font-extrabold text-black dark:text-white ">Crafting a Sustainable
-     <div className="ransition delay-150 duration-300 ease-in-out active:bg-green-400 hover:text-white">
+     <div className="ransition delay-150 duration-300 ease-in-out inline bg-green-400 hover:text-white">
       Future
       </div>
      </h2>
